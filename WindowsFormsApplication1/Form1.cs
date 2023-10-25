8fc65de48746708869707e7bb836b767f5d62397
Hien <nguyenthehien140803@gmail.com>
2023-10-25 16:15:52 +0700

    update

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Ketnoi kn = new Ketnoi();
+        SqlConnection connsql;
+        public Form1()
+        {
+            InitializeComponent();
+            connsql = kn.connect;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connsql.State == ConnectionState.Closed)
+                {
+                    connsql.Open();
+                }
+                string insertString;
+                insertString = "insert into Khoa values('" + textBox1.Text + "',N'" + textBox2.Text + "')";
+                SqlCommand cmd = new SqlCommand(insertString,connsql);
+                cmd.ExecuteNonQuery();
+                if(connsql.State == ConnectionState.Open)
+                {
+                    connsql.Close();
+                }
+                MessageBox.Show("Thanh cong");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("That bai");
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connsql.State == ConnectionState.Closed)
+                {
+                    connsql.Open();
+                }
+                string deleteString;
+                deleteString = "DELETE KHOA WHERE MAKHOA='" + textBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(deleteString, connsql);
+                cmd.ExecuteNonQuery();
+                if (connsql.State == ConnectionState.Open)
+                {
+                    connsql.Close();
+                }
+                MessageBox.Show("Thanh cong");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("That bai");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connsql.State == ConnectionState.Closed)
+                {
+                    connsql.Open();
+                }
+                string updateString;
+                updateString = "update KHOA set TENKHOA='" + textBox2.Text + "' where MAKHOA = '" + textBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(updateString, connsql);
+                cmd.ExecuteNonQuery();
+                if (connsql.State == ConnectionState.Open)
+                {
+                    connsql.Close();
+                }
+                MessageBox.Show("Thanh cong");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("That bai");
+            }
+        }   
+    }
+}
---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public class Ketnoi
+    {
+        public SqlConnection connect;
+        public Ketnoi()
+        {
+                connect = new SqlConnection("Data Source=A209PC12;Initial Catalog=QLKHOA;Integrated Security = True");
+        }
+        public Ketnoi(string strcn)
+        {
+            connect = new SqlConnection(strcn);
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
